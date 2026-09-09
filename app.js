44e1e48b2b2b714e9516048fdb7176dc0a290a83
valorapayment <cbale7218@gmail.com>
2026-09-09 06:55:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const GIST_RAW_URL = 'https://gist.githubusercontent.com/cbale7218-bot/c9fb99d723dffd3436f25f1dfc54c4da/raw/gistfile1.txt';
+const GIST_RAW_URL = 'https://gist.githubusercontent.com/cbale7218-bot/7c21a30b5a4f1a8a3892ff8236236a58/raw/49201a22aa1df5534eb6247566f2e7af1024559e/gistfile1.txt';
 const REFRESH_MS = 5000;
 
 const fallback = {
